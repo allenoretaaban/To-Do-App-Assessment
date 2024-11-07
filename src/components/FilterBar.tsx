4cf382e8d528fddca5a6b188e9c6fcdca50eebc0
Allen Aban <aban.allen@gmail.com>
2024-11-07 11:23:02 +0800

    adjustments

--- src/components/FilterBar.tsx
+++ src/components/FilterBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
 import styles from '../helpers/styles';
 import TaskButton from './TaskButton';
